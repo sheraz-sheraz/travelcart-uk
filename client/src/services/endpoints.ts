267515c1534e84b4a76d46589f49c63f63cf4438
sheraz-sheraz <sherazahmed14@hotmail.com>
2025-03-05 02:33:21 +0500

    url changed

--- client/src/services/endpoints.ts
+++ client/src/services/endpoints.ts
@@ -2,7 +2,7 @@ const API_ENDPOINTS = {
   getUsers: "/users",
   getTrips: "/trips",
   createBooking: "/bookings",
-  sendEmail:'/contact/send-email'
+  sendEmail:'/api/contact/send-email'
 };
 
 export default API_ENDPOINTS;
