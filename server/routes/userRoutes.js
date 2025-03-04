16a587a782c63f862b244bd5b0dd04a757e4eded
sheraz-sheraz <sherazahmed14@hotmail.com>
2025-03-05 01:55:02 +0500

    backend removed

--- server/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const {createUser, getUsers} = require('../controllers/userController');
-const router = express.Router();
-
-router.post("/users", createUser)
-router.get("/users", getUsers)
-
-module.exports = router;
