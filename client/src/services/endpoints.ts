52d1b2b92632c681d093b9fb5057898fd824eb99
sheraz-sheraz <sherazahmed14@hotmail.com>
2025-03-17 23:32:45 +0500

    booking apis

--- client/src/services/endpoints.ts
+++ client/src/services/endpoints.ts
@@ -2,7 +2,10 @@ const API_ENDPOINTS = {
   getUsers: "/users",
   getTrips: "/trips",
   createBooking: "/bookings",
-  sendEmail:'/api/contact/send-email'
+  sendEmail:'/api/contact/send-email',
+  FLIGHT_OFFERS: "/api/flights/flight-offers",
+  CONFIRM_FLIGHT_OFFERS: "/api/flights/confirm-flight-offer",
+
 };
 
 export default API_ENDPOINTS;
